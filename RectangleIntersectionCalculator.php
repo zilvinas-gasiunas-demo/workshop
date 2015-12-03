88a0df955cc1d71eb87cd8198b9d8694f6c72130
AndriuxLaptop <andriux959@gmail.com>
2015-12-03 12:23:58 +0200

    Added functions

--- RectangleIntersectionCalculator.php
+++ RectangleIntersectionCalculator.php
@@ -1,8 +1,21 @@
 <?php
 
 	class RectangleIntersectionCalculator {
-	
-	
+		
+		public function RectangleIntersectionCalculator(Rectangle rectangle) {
+		
+		}
+		
+		public function generic(Geometry geometry) {
+		
+		}
+		public function rectangle(Rectangle rectangle) {
+		
+		}
+		public function circle(Circle circle) {
+		
+		}
+		
 	}
 
 	?>
