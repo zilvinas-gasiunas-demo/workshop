5cb3484fa3e81a6f9cd3858995343a1c96455b8b
uberp <pauliusvir@gmail.com>
2015-12-03 12:49:51 +0200

    idiot

--- Paulius.php
+++ Paulius.php
@@ -1,2 +1,10 @@
 <?php
 class paulius
+ { 
+    public $aMemberVar = 'aMemberVar Member Variable';    
+	public $aFuncName = 'aMemberFunc'; 
+    
+    
+    function aMemberFunc() { 
+        print 'Inside `aMemberFunc()`'; 
+    } 
